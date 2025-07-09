c484b70d684bfc9cbef6dfab737b4b9ae191ff1f
Tom Quarendon <tom@quarendon.net>
2025-07-09 13:10:26 +0100

    preserve

--- src/components/EventManager.tsx
+++ src/components/EventManager.tsx
@@ -1,7 +1,6 @@
-import { Button, Card, Collection, Flex } from '@aws-amplify/ui-react';
 import { generateClient } from 'aws-amplify/data';
 import type { Schema } from '../../amplify/data/resource'; 
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import TableContainer from '@mui/material/TableContainer';
 import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
@@ -9,29 +8,86 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import TableCell from '@mui/material/TableCell';
 import TableBody from '@mui/material/TableBody';
+import Collapse from '@mui/material/Collapse';
+import IconButton from '@mui/material/IconButton';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Chip from '@mui/material/Chip';
 
 const client = generateClient<Schema>();
 
+function Row(props: { event: RowData }) {
+  const { event } = props;
+  const [open, setOpen] = useState(false);
+  
+  return (
+    <React.Fragment>
+      <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
+        <TableCell>
+          <IconButton
+            aria-label="expand row"
+            size="small"
+            onClick={() => {
+              setOpen(!open)
+            }}
+          >
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
+        <TableCell>
+          {new Date(event.startDate).toLocaleDateString(undefined, {dateStyle: "full"})}
+        </TableCell>
+        <TableCell>{/*event.eventType*/}</TableCell>
+        <TableCell>{event.location.name}</TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Box sx={{ margin: 1 }}>
+              <Typography variant="h6" gutterBottom component="div">
+                Attendees
+              </Typography>
+              <Stack>
+                {event.cars.map((car)=>(
+                  <Chip key={car.id} variant='outlined' label={car.car.name}/>
+                ))}
+              </Stack>
+            </Box>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </React.Fragment>
+  );
+}
+
+interface RowData {
+  readonly id : string
+  readonly startDate : string
+  // readonly eventType : "Trackday" | "Raceday" | "Testing" | null
+  readonly location: {
+      readonly name: string;
+  };
+  readonly cars: {
+    readonly id: string
+      readonly car: {
+          readonly name: string;
+      };
+  }[];
+}
+
 export default function EventManager() {
-  const [events, setEvents] = useState<Array<Schema["Event"]["type"]>>([]);
-  const [locations, setLocations] = useState<Map<string, string>>(new Map());
+  const [events, setEvents] = useState<Array<RowData>>([]);
 
   useEffect(() => {
-    const sub = client.models.Event.observeQuery().subscribe({
-      next: (data) => setEvents([...data.items]),
-    });
-    return () => sub.unsubscribe();
-  }, []);
-
-  useEffect(() => {
-    const sub = client.models.Location.observeQuery().subscribe({
-      next: (data) => {
-        const locationMap = new Map<string, string>();
-        data.items.forEach(location => {
-            locationMap.set(location.id, location.name)
-        })
-        setLocations(locationMap)
+    const sub = client.models.Event.observeQuery(
+      {
+        selectionSet: ["id", /*"eventType",*/ "startDate", "location.name", "cars.id", "cars.car.name"],
       }
+    ).subscribe({
+      next: (data) => setEvents([...data.items]),
     });
     return () => sub.unsubscribe();
   }, []);
@@ -48,16 +104,7 @@ export default function EventManager() {
         </TableHead>
         <TableBody>
           {events.map((event) => (
-            <TableRow
-              key={event.id}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell>
-                {new Date(event.startDate).toLocaleDateString(undefined, {dateStyle: "full"})}
-              </TableCell>
-              <TableCell>{event.eventType}</TableCell>
-              <TableCell>{locations.get(event.locationId) || event.locationId}</TableCell>
-            </TableRow>
+            <Row key={event.id} event={event} />          
           ))}
         </TableBody>
       </Table>      
